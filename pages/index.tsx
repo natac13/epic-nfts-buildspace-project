d99bfa9644bf432dc36975e45ed9ab4794d0c499
Sean Campbell <sean.campbell13@gmail.com>
2021-10-17 21:36:58 -0400

    chore: clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,17 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  CircularProgress,
-  Container,
-  TextField,
-  Typography,
-} from '@mui/material'
-import * as React from 'react'
+import { Box, CircularProgress, Container, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
-import { ethers } from 'ethers'
-import ContractAbi from '../src/WavePortal.json'
+import * as React from 'react'
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const contractABI = ContractAbi.abi
-const oldContractAddress = '0xb31fA22DcDEb2b2a3d48EaC448d0cd3465D08433'
-const contractAddress = '0x50049107E263e5890A061C304f25216bA2624fB0'
 
 export default function Index() {
   const [currentAccount, setCurrentAccount] = React.useState('')
   const [loading, setLoading] = React.useState(false)
-  const [latestCount, setLastestCount] = React.useState(0)
-  const [allWaves, setAllWaves] = React.useState([])
-  const [message, setMessage] = React.useState('')
-  const [error, setError] = React.useState('')
 
-  const onChange = (str: string): void => {
-    setError('')
-    setMessage(str)
-  }
+  // const onChange = (str: string): void => {
+  //   setError('')
+  //   setMessage(str)
+  // }
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -80,127 +62,8 @@ export default function Index() {
     }
   }
 
-  const getWaveCount = async () => {
-    try {
-      const { ethereum } = window
-      console.log(process.env.CONTRACT_ADDRESS)
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const wavePortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        )
-
-        const count = await wavePortalContract.getTotalWaves()
-        setLastestCount(count?.toNumber())
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const wave = async () => {
-    try {
-      const { ethereum } = window
-
-      if (ethereum && message) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const wavePortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        )
-
-        /*
-         * Execute the actual wave from your smart contract
-         */
-        const waveTxn = await wavePortalContract.wave(message, {
-          gasLimit: 300_000,
-        })
-        setLoading(true)
-
-        await waveTxn.wait()
-        setLoading(false)
-        setMessage('')
-        setError('')
-
-        const count = await wavePortalContract.getTotalWaves()
-        setLastestCount(count?.toNumber())
-      } else {
-        setError('You need to include a message.')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  /*
-   * Create a method that gets all waves from your contract
-   */
-  const getAllWaves = async () => {
-    try {
-      const { ethereum } = window
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const wavePortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        )
-
-        /*
-         * Call the getAllWaves method from your Smart Contract
-         */
-        const waves = await wavePortalContract.getAllWaves()
-
-        console.log({ waves })
-        /*
-         * We only need address, timestamp, and message in our UI so let's
-         * pick those out
-         */
-        const wavesCleaned = waves.map((wave) => {
-          return {
-            address: wave.waver,
-            timestamp: new Date(wave.timestamp * 1000),
-            message: wave.message,
-          }
-        })
-
-        /*
-         * Store our data in React State
-         */
-        setAllWaves(wavesCleaned)
-
-        wavePortalContract.on('NewWave', (from, timestamp, message) => {
-          console.log('NewWave', from, timestamp, message)
-
-          setAllWaves((prevState) => [
-            ...prevState,
-            {
-              address: from,
-              timestamp: new Date(timestamp * 1000),
-              message: message,
-            },
-          ])
-        })
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   React.useEffect(() => {
     checkIfWalletIsConnected()
-    getWaveCount()
   }, [])
 
   return (
