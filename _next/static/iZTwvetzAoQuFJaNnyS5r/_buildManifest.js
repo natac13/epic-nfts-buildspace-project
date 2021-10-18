09e93395b2b565b6df1d8c9087669eba3a30ebc6
natac13 <natac13@users.noreply.github.com>
2021-10-18 01:56:23 +0000

    Deploying to gh-pages from @ natac13/epic-nfts-buildspace-project@525001e796b5d2223da376324aa244493a798c4d 🚀

---
+++ _next/static/iZTwvetzAoQuFJaNnyS5r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-94c3c807b4f49e2108e9.js","static/chunks/pages/index-b7039cab044c7ed333ab.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
